c5e555607dfc092ecdfa613340ecaab03d689c86
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-08-01 13:46:06 +0530

    express server setup --done

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // setup mongodb database connection
 const mongoose= require("mongoose")
-const { MONGODB_URL } = require("./utils/config")
+const { MONGODB_URL, PORT, HOST } = require("./utils/config")
+const app = require("./app")
+const { error } = require("node:console")
 
 
 mongoose
 .connect(MONGODB_URL)
 .then(() => {
     console.log("Connected to MongoDB")
+
+    app
+    .listen(PORT, HOST, () => {
+        console.log(`Server is running on http://${HOST}:${PORT}`)
+    })
+    .on(error,(error) => {
+        console.log('Error to server connecting', error.message)
+    })
 })
 .catch((error) => {
      console.log('Error connecting to MongoDB', error.message)
