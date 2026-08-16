4eea3495088906ef856912a0ea45a2c9a8cd8b0d
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-08-16 22:05:49 +0530

    update cros

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use("/uploads", express.static('uploads'))
 
 // enable cros
 app.use(cros({
-    origin: "http://localhost:5173",//repace with your frontend URL
+    origin: "http://fe-fsdcweeb4142-delivo.netlify.app",//repace with your frontend URL
     credentials: true //allow cookies to be sent
 }))
 
